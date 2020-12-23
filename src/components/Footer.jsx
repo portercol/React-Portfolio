346c6b0d08ce742012b620cbb15a38306d25669f
Collin Porter <colls.potta@gmail.com>
2020-12-23 13:06:44 -0700

    import React, created functional component to export out, created footer data with link to github, footer text

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return <footer className="footer mt-5">
+        <div className="container-fluid">
+            <div className="d-flex justify-content-center">
+                <h5 className="text-light">site designed by <a href="https://github.com/portercol" target="_blank" rel="noopener noreferrer">Collin Porter</a></h5>
+            </div>
+            <p className="text-light">&copy; Copyright 2020</p>
+        </div>
+    </footer>;
+}
+
+export default Footer;
