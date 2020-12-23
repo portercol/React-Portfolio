b5285ca33afdc2625ab8758ec0910395a1f83af0
Collin Porter <colls.potta@gmail.com>
2020-12-23 12:16:18 -0700

    imported components, pages, and react-router-dom elements, created path routes enclosed with header and footer componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Portfolio from "./components/pages/Portfolio";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
+  return ( 
+    <Router>
+        <Header/>
+        <Route exact path="/" component={About}/>
+        <Route exact path="/portfolio" component={Portfolio}/>
+        <Route exact path="/contact" component={Contact}/>
+        <Footer/>
+    </Router>
   );
 }
 
