6f62f52d78218ffb6f451e80370aa7544771b894
Collin Porter <colls.potta@gmail.com>
2020-12-24 11:39:52 -0700

    imported React and Button componenet, creatd About functional component, created card to hold about me data, horizontal rules and spots for card body and text, added button to navigate to portfolio page, exported About component

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "../Button";
+
+function About() {
+    
+    return (
+        <div className="container">
+            <div className="row d-flex justify-content-around">
+                <div className="card mt-3 p-4 w-50">
+                    <h1 className="card-title text-center">Collin Porter</h1>
+                    <hr/>
+                    <div className="d-flex justify-content-center">
+                    </div>
+                    <hr/>
+                    <div className="card-body text-center">
+                    </div>
+                </div>
+            </div>
+            <Button btnText="portfolio" link={"/portfolio"}/>
+        </div>
+    );
+}
+
+export default About;
