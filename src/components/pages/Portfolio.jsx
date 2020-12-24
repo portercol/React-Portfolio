7c1c75ad70ec299d71a84d859268ad6d139c636b
Collin Porter <colls.potta@gmail.com>
2020-12-24 11:55:56 -0700

    added div tags to contain portfolio card data, using .map to map over the project data I used the properties from the json file to populate the card with the properties used for each project

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -6,7 +6,26 @@ import Button from "../Button";
 function Portfolio() {
     
     return (
-        
+        <div className="container">
+            <div className="row justify-content-around">
+                {projectData.map(project => {
+                    return (
+                        <Project
+                            key={project.id}
+                            description={project.description} 
+                            id={project.id}
+                            img={project.img} 
+                            link={project.link} 
+                            name={project.name} 
+                            repo={project.repo} 
+                            tech={project.tech}
+                            >
+                        </Project>
+                    );
+                })}
+            </div>
+            <Button btnText="contact" link={"/contact"}/>
+        </div>
     )
 }
 
