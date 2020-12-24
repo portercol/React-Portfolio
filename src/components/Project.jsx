55d479e8053721074309a762024904a6c8c2fd62
Collin Porter <colls.potta@gmail.com>
2020-12-24 11:22:39 -0700

    added horizontal rule, p tags for card text using props descriptions and tech stuff, added buttons to view the project repo and live site

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,6 +10,15 @@ function Project(props) {
             <div className="card-title mt-3 mb-0">
                 <h5>{props.name}</h5>
             </div>
+            <hr/>
+            <div className="card-body mt-0 pt-0">
+                <p className="card-text mb-3">{props.description}</p>
+                <p className="card-text" style={{color: "black"}}><strong>Technologies: </strong>{props.tech}</p>
+            </div>
+            <div className="row btn-group">
+                <a data-click={props.name} href={props.repo} className="btn btn-dark mr-1 ml-3" target="_blank" rel="noopener noreferrer">View Repo</a>
+                <a data-click={props.name} href={props.link} className="btn btn-dark ml-1 mr-3" target="_blank" rel="noopener noreferrer">Live Site</a>
+            </div>
         </div>
       </>
     )
