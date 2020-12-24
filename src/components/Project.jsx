a16e7dbecb37f472dc5674a17e4e82c2e4fcdd7b
Collin Porter <colls.potta@gmail.com>
2020-12-24 11:09:36 -0700

    added div for project name using props

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,7 +7,9 @@ function Project(props) {
             <a data-click={props.name} href={props.link} target="_blank" rel="noopener noreferrer">
                 <img src={props.img} className="card-img-top" alt={props.name}></img>
             </a>
-            
+            <div className="card-title mt-3 mb-0">
+                <h4>{props.name}</h4>
+            </div>
         </div>
       </>
     )
