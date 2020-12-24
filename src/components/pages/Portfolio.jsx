b03b8121251cae9994c1da75996b1f5fa7bc02de
Collin Porter <colls.potta@gmail.com>
2020-12-24 11:50:33 -0700

    imported React, button/project components and project data, created functional portfolio component

---
+++ src/components/pages/Portfolio.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Project from "../Project";
+import projectData from "../projectData.json";
+import Button from "../Button";
+
+function Portfolio() {
+    
+    return (
+        
+    )
+}
+
+export default Portfolio;
